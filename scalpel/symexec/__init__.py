a881ab434389e8b0f8363b1bd0095d31d955c78f
Jiawei Wang <jiawei.wang1@monash.edu>
2021-12-10 23:24:06 +1100

    push the first file for the symbolic execution feature

---
+++ scalpel/symexec/__init__.py
@@ -0,0 +1,9 @@
+"""
+symexec is the symbolic execution  feature in Scalpel. Its implementation  is based on the paper [1] , PyExZ3
+[2] andCrossHair[3] projects.
+References:
+[1] [Bruni, A. D., Disney, T., & Flanagan, C. (2011). A peer architecture for lightweight symbolic execution. Universidad de California, Santa Cruz.](https://hoheinzollern.files.wordpress.com/2008/04/seer1.pdf)
+[2] [PyExZ3](https://github.com/thomasjball/PyExZ3)
+[3] [CrossHair](https://github.com/pschanely/CrossHair)
+"""
+
